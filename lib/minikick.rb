b2c66f2b0b8142fa1d4b2b0a6d20a67e91a6dbd7
Rachel King <rachel.b.king@gmail.com>
2016-07-06 15:48:27 -0400

    Create basic Project class (without validation).

--- lib/minikick.rb
+++ lib/minikick.rb
@@ -1,5 +1,25 @@
+require 'money'
+require 'monetize'
+
 require "minikick/version"
 
-module Minikick
-  # Your code goes here...
+Money.use_i18n = false
+
+class Project
+
+	attr_reader :name
+
+	def initialize(name, amount)
+		@name = name
+		@amount = amount.to_money
+	end
+
+	def amount
+		return @amount.format(
+			no_cents_if_whole: true,
+			thousands_separator: false,
+			symbol: false
+		)
+	end
+
 end
