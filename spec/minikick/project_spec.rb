f66f8eb470cfa5ee3f2d9c9e9c01d29af7b6f947
Rachel King <rachel.b.king@gmail.com>
2016-07-06 18:35:17 -0400

    Standardize specs.

--- spec/minikick/project_spec.rb
+++ spec/minikick/project_spec.rb
@@ -12,19 +12,6 @@ describe 'MiniKick::Project' do
 		expect(new_project.amount).to eq(default_project_amount)
 	end
 
-	it 'raises an ArgumentError if a project name contains invalid characters' do
-		invalid_character_error_message = 'Project name may only use alphanumeric characters, underscores, and dashes.'
-		
-		expect { Minikick::Project.new('Fancy.Cooler', default_project_amount) }.to raise_error(ArgumentError, invalid_character_error_message)
-	end
-
-	it 'raises an ArgumentError if a project name is less than 4 characters or more than 20' do
-		invalid_length_error_message = 'Project name must be between 4 and 20 characters.'
-		
-		expect { Minikick::Project.new('Fan', default_project_amount) }.to raise_error(ArgumentError, invalid_length_error_message)
-		expect { Minikick::Project.new('The_Fanciest_Of_Coolers', default_project_amount) }.to raise_error(ArgumentError, invalid_length_error_message)
-	end
-
 	it 'allows both dollars and cents in the project amount' do
 		project_amount = '1000.99'
 		new_project = Minikick::Project.new(default_project_name, project_amount)
@@ -32,11 +19,28 @@ describe 'MiniKick::Project' do
 		expect(new_project.amount).to eq(project_amount)
 	end
 
-	it 'raises an ArgumentError if a project amount contains "$"' do
+	it 'raises an error if a project name contains invalid characters' do
+		invalid_character_error_message = 'Project name may only use alphanumeric characters, underscores, and dashes.'
+		
+		expect { Minikick::Project.new('Fancy.Cooler', default_project_amount) }
+			.to raise_error(invalid_character_error_message)
+	end
+
+	it 'raises an error if a project name is less than 4 characters or more than 20' do
+		invalid_length_error_message = 'Project name must be between 4 and 20 characters.'
+		
+		expect { Minikick::Project.new('LoL', default_project_amount) }
+			.to raise_error(invalid_length_error_message)
+		expect { Minikick::Project.new('Ladies_of_Literature_Volume_2', default_project_amount) }
+			.to raise_error(invalid_length_error_message)
+	end
+
+	it 'raises an error if the project amount contains "$"' do
 		project_amount = '$1000'
 		invalid_amount_message = 'Project amount must not contain the "$" character.'
 
-		expect { Minikick::Project.new(default_project_name, project_amount) }.to raise_error(ArgumentError, invalid_amount_message)
+		expect { Minikick::Project.new(default_project_name, project_amount) }
+			.to raise_error(invalid_amount_message)
 	end
 
 end
