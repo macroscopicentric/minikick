cffc5fe017630b203de67e6e5185a3249f7dd263
Rachel King <rachel.b.king@gmail.com>
2016-07-20 17:49:05 -0400

    Add Minikick class and specs.

--- lib/minikick.rb
+++ lib/minikick.rb
@@ -1,7 +1,77 @@
+#!/usr/bin/env ruby
+require 'json'
 
 class Minikick
 
 	require 'minikick/project'
 	require 'minikick/user'
+
+	FILENAME = 'minikick_data.txt'
+
+	attr_reader :users
+	attr_reader :projects
+
+	def initialize(filename=FILENAME)
+		@filename = filename
+		@users = {}
+		@projects = {}
+	end
+
+	def self.save(filename=FILENAME)
+		File.open(filename, 'wb') {|f| f.write(Marshal.dump(self)) }
+	end
+
+	def self.load(filename=FILENAME)
+		return Marshal.load(File.binread(filename))
+	end
+
+	def self.load_or_new(filename=FILENAME)
+		File.exists?(filename) ? minikick = self.load(filename) : minikick = self.new
+		return minikick
+	end
+
+	def add_project(project_name, target_amount)
+		project = Minikick::Project.new(project_name, target_amount)
+		@projects[project_name] = project
+
+		success_message = "Added #{project.name} with a target of $#{project.amount}"
+		return success_message
+	end
+
+	def back_project(user_name, project_name, credit_card_number, backing_amount)
+		user = Minikick::User.new(user_name, credit_card_number)
+		@users[user_name] = user
+		user.add_project_for_amount(project_name, backing_amount)
+
+		project = @projects[project_name]
+		project.add_backer_and_amount(user_name, backing_amount)
+
+		success_message = "#{user.name} backed project #{project.name} for $#{backing_amount}."
+		return success_message
+	end
+
+	def list_backers(project_name)
+		project = @projects[project_name]
+
+		info = project.backers_and_amounts.map do |backer, amount|
+			"-- #{backer} backed for $#{amount}."
+		end
+
+		successful_backing = "#{project.name} is successful!"
+		not_yet_backed = "#{project.name} needs $#{project.funding_needed} more dollars to be successful."
+		project.funded? ? info << successful_backing : info << not_yet_backed
+
+		return info
+	end
+
+	def list_projects_backed(user_name)
+		user = @users[user_name]
+
+		info = user.backed_projects.map do |project, amount|
+			"-- Backed #{project} for $#{amount}"
+		end
+
+		return info
+	end
 
 end
--- spec/minikick_spec.rb
+++ spec/minikick_spec.rb
@@ -1,5 +1,73 @@
 require 'spec_helper'
 
 describe 'Minikick' do
+
+	before(:each) do
+		Minikick::User.credit_cards.clear
+	end
+
+	let(:default_project_name) { 'fun_new_project' }
+	let(:default_project_amount) { '500' }
+	let(:default_user_name) { 'Lillian_Schwartz' }
+	let(:default_credit_card) { '4111111111111111' }
+	let(:default_backing_amount) { '20' }
+
+	it 'can add a new project' do
+		minikick = Minikick.new
+
+		expect { minikick.add_project(default_project_name, default_project_amount) }.
+			to change { minikick.projects.length }.by( 1 )
+		expect( minikick.projects[default_project_name] ).to be_a( Minikick::Project )
+	end
+
+	it 'can back a project' do
+		minikick = Minikick.new
+		minikick.add_project(default_project_name, default_project_amount)
+
+		expect { minikick.back_project(default_user_name, default_project_name, default_credit_card, default_backing_amount) }.
+			to change { minikick.users.length }.by( 1 )
+		expect( minikick.users[default_user_name] ).to be_a( Minikick::User )
+	end
+
+	it 'can list the backers of a project' do
+		minikick = Minikick.new
+		minikick.add_project(default_project_name, default_project_amount)
+		minikick.back_project(default_user_name, default_project_name, default_credit_card, default_backing_amount)
+
+		response = minikick.list_backers(default_project_name)
+
+		expect( response ).to be_an( Array )
+		expect( response.length ).to eq( 2 )
+		expect( response.first ).to include( default_user_name )
+		expect( response.first ).to include( default_backing_amount )
+		expect( response.last ).to include( default_project_name )
+	end
+
+	it 'knows when a project has been successfully backed' do
+		minikick = Minikick.new
+		minikick.add_project(default_project_name, default_project_amount)
+		minikick.back_project(default_user_name, default_project_name, default_credit_card, default_project_amount)
+
+		response = minikick.list_backers(default_project_name)
+
+		expect( response ).to be_an( Array )
+		expect( response.length ).to eq( 2 )
+		expect( response.first ).to include( default_user_name )
+		expect( response.first ).to include( default_project_amount )
+		expect( response.last ).to include( 'successful' )
+	end
+
+	it 'can list the projects a user has backed' do
+		minikick = Minikick.new
+		minikick.add_project(default_project_name, default_project_amount)
+		minikick.back_project(default_user_name, default_project_name, default_credit_card, default_backing_amount)
+
+		response = minikick.list_projects_backed(default_user_name)
+
+		expect( response ).to be_an( Array )
+		expect( response.length ).to eq( 1 )
+		expect( response.first ).to include( default_project_name )
+		expect( response.first ).to include( default_backing_amount )
+	end
 	
 end
